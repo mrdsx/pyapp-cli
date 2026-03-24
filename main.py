b73a6ca1fb174f4f81df4f0dc99b464f2c8affe1
mrdsx <andre0.castlegames@gmail.com>
2026-03-24 14:43:06 +0300

    feat: add django setup and colorized logging

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import shutil
 import subprocess
 import sys
+from typing import Literal
 
+from colorama import Fore, Style, init
 import fire
 from InquirerPy import prompt
 
@@ -14,26 +16,29 @@ venv_dir = ".venv"
 
 
 class ProjectGenerator:
-    # frameworks that has to be installed as packages
+    # frameworks without built-in CLI for scaffolding the project
     _framework_packages = set(["FastAPI", "Flask"])
     _templates_dir = os.path.abspath("templates")
 
     def init(self):
+        init(autoreset=True)
         raw_answers = prompt(questions)
         answers = Answers.model_validate(raw_answers)
 
-        self._create_project_folder(answers.project_path)
+        self._create_project_folder(answers.project_path, answers.source_folder)
 
-        if answers.package_manager == "poetry":
-            self._handle_poetry_setup()
+        if answers.package_manager == "pip":
+            self._ensure_pip_installation()
+        elif answers.package_manager == "poetry":
+            self._ensure_poetry_installation()
         elif answers.package_manager == "uv":
-            self._handle_uv_setup()
+            self._ensure_uv_installation()
 
         if answers.framework == "None" or answers.framework in self._framework_packages:
             self._create_main_file(answers.source_folder, answers.framework)
 
         dependencies = answers.libraries
-        if answers.framework in self._framework_packages:
+        if answers.framework != "None":
             dependencies.append(self._framework_id(answers.framework))
 
         if answers.package_manager == "pip":
@@ -43,71 +48,53 @@ class ProjectGenerator:
         elif answers.package_manager == "uv":
             self._uv_setup_project(dependencies, answers.python_version)
 
-        print("Finished! Enjoy the project :)")
+        if answers.framework == "Django":
+            self._django_setup_project(answers.package_manager, answers.source_folder)
+
+        print(Fore.GREEN + "Finished! Enjoy the project :)")
 
     def _framework_id(self, framework: str) -> str:
         return framework.lower()
 
-    def _create_project_folder(self, project_path: str) -> None:
-        print(f"Creating folder '{project_path}'.")
+    def _missing_dependency_message(self, dependency: str) -> str:
+        return Fore.RED + (
+            f"Can't detect installed {dependency}.\n"
+            f"Ensure {dependency} is installed and then run the script again."
+        )
+
+    def _create_project_folder(self, project_path: str, source_folder: str) -> None:
+        print(Style.DIM + f"Creating folder '{project_path}'...")
         Path(project_path).mkdir(exist_ok=True)
+        if source_folder != "root":
+            Path(project_path, source_folder).mkdir(exist_ok=True)
         os.chdir(project_path)
 
-    def _handle_poetry_setup(self) -> None:
+    def _ensure_pip_installation(self) -> None:
+        pip_exists = shutil.which("pip") is not None
+        if pip_exists:
+            print(Fore.GREEN + "pip is already installed.")
+        else:
+            print(self._missing_dependency_message("pip"))
+            exit(127)
+
+    def _ensure_poetry_installation(self) -> None:
         poetry_exists = shutil.which("poetry") is not None
         if poetry_exists:
-            print("Poetry is already installed.")
+            print(Fore.GREEN + "Poetry is already installed.")
         else:
-            print("Installing pipx...")
-            subprocess.check_call(
-                [
-                    "python",
-                    "-m",
-                    "pip",
-                    "install",
-                    "--user",
-                    "pipx",
-                    "--break-system-packages",
-                ],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-            print("Installing poetry...")
-            subprocess.check_call(
-                ["pipx", "install", "poetry", "--force"],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-            print("Poetry is installed.")
+            print(self._missing_dependency_message("Poetry"))
+            exit(127)
 
-    def _handle_uv_setup(self) -> None:
+    def _ensure_uv_installation(self) -> None:
         uv_exists = shutil.which("uv") is not None
         if uv_exists:
-            print("uv is already installed.")
+            print(Fore.GREEN + "uv is already installed.")
         else:
-            print("Installing pipx...")
-            subprocess.check_call(
-                [
-                    "python",
-                    "-m",
-                    "pip",
-                    "install",
-                    "--user",
-                    "pipx",
-                    "--break-system-packages",
-                ],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-            print("Installing uv...")
-            subprocess.check_call(
-                ["pipx", "install", "uv", "--force"],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-            print("uv is installed.")
+            print(self._missing_dependency_message("uv"))
+            exit(127)
 
     def _create_main_file(self, source_folder: str, framework: str) -> None:
+        print(Style.DIM + "Creating main.py file...")
         python_file = None
         if source_folder == "root":
             python_file = Path("main.py")
@@ -130,7 +117,7 @@ class ProjectGenerator:
                 f.write(project_template)
 
     def _pip_setup_project(self, dependencies: list[str]) -> None:
-        print("Creating virtual environment...")
+        print(Style.DIM + "Creating virtual environment...")
         subprocess.check_call(
             [
                 sys.executable,
@@ -143,16 +130,24 @@ class ProjectGenerator:
         )
 
         if len(dependencies) > 0:
-            print("Installing dependencies...")
+            print(Style.DIM + "Installing dependencies...")
             python_executable = os.path.abspath(
                 os.path.join(venv_dir, "bin", "python3")
             )
             subprocess.check_call(
                 [python_executable, "-m", "pip", "install", *dependencies],
             )
+            result = subprocess.run(
+                [python_executable, "-m", "pip", "freeze"],
+                check=True,
+                capture_output=True,
+                text=True,
+            )
+            with open("requirements.txt", "w") as f:
+                f.write(result.stdout)
 
     def _poetry_setup_project(self, dependencies: list[str]) -> None:
-        print("Initializing project...")
+        print(Style.DIM + "Initializing project...")
         subprocess.check_call(
             ["poetry", "init", "-n"],
             stdout=subprocess.DEVNULL,
@@ -160,7 +155,7 @@ class ProjectGenerator:
         )
 
         if len(dependencies) > 0:
-            print("Installing dependencies...")
+            print(Style.DIM + "Installing dependencies...")
             custom_env = os.environ.copy()
             custom_env["POETRY_VIRTUALENVS_IN_PROJECT"] = "true"
             custom_env["VIRTUAL_ENV"] = os.path.abspath(venv_dir)
@@ -174,14 +169,18 @@ class ProjectGenerator:
             )
 
     def _uv_setup_project(self, dependencies: list[str], python_version: str) -> None:
-        print("Initializing project...")
-        subprocess.check_call(["uv", "init", "--bare", "--no-workspace"])
+        print(Style.DIM + "Initializing project...")
+        subprocess.check_call(
+            ["uv", "init", "--bare", "--no-workspace"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
 
         with open(".python-version", "w") as f:
             f.write(python_version)
 
         if len(dependencies) > 0:
-            print("Installing dependencies...")
+            print(Style.DIM + "Installing dependencies...")
             subprocess.check_call(
                 [
                     "uv",
@@ -189,6 +188,23 @@ class ProjectGenerator:
                     *dependencies,
                 ],
             )
+
+    def _django_setup_project(
+        self, package_manager: Literal["pip", "poetry", "uv"], source_folder: str
+    ) -> None:
+        print(Style.DIM + "Initializing Django project...")
+
+        python_executable = os.path.abspath(os.path.join(venv_dir, "bin", "python3"))
+        django_util = [python_executable, "-m", "django"]
+        if package_manager == "uv":
+            django_util = ["uv", "run", "django-admin"]
+
+        if source_folder != "root":
+            os.chdir(source_folder)
+
+        subprocess.check_call(
+            [*django_util, "startproject", "mysite", "."],
+        )
 
 
 if __name__ == "__main__":
